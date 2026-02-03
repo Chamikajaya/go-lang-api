1a63ffc9b4fad70e6336334c54af0f72d9e679f6
Chamika Jayasinghe <chamika.21@cse.mrt.ac.lk>
2026-02-03 16:14:06 +0530

    implemented the config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,49 @@
 package config
 
-type Config struct {}
+import (
+	"fmt"
+	"os"
+)
+
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	ServerPort string
+}
+
+func LoadConfig() (*Config, error) {
+
+	config := &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "user_management"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+
+	return config, nil
+}
+
+// belongs to the Config struct - method on Config struct (receiver function)
+func (c *Config) GetDatabaseURL() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName,
+	)
+}
+
+// private - unexported
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+
